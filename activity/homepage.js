7daf102c2e8070e0c7c8e5f2970d9a1ed58a9d8e
Vikas Kumar <vk.pratap98@gmail.com>
2021-04-28 04:39:28 +0530

    extracted all matches result url using web-scrapping

--- activity/homepage.js
+++ activity/homepage.js
@@ -2,6 +2,14 @@ const request = require('request');
 const fs = require('fs');
 const cheerio = require('cheerio')
 
-request("https://www.espncricinfo.com/series/ipl-2020-21-1210595" , (err, res, body)=>{
-    fs.writeFileSync('./homepage.html', body);
-} );
+// request("https://www.espncricinfo.com/series/ipl-2020-21-1210595" , (err, res, body)=>{
+//     fs.writeFileSync('./homepage.html', body);
+// } );
+
+let homepage = fs.readFileSync("./homepage.html");
+
+let ch = cheerio.load(homepage);
+
+let aTag = ch(".widget-items.cta-link a");
+let allMatchesResults = "https://www.espncricinfo.com"+aTag['0'].attribs.href;
+console.log(allMatchesResults);
